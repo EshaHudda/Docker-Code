c40cb416e77e7230b1a473e248966c3cf8970139
Esha.Hudda <eshuhudda27@gmail.com>
2025-08-02 20:42:20 +0000

    Added my docker code

---
+++ java-app image/src/Main/java/Main.java
@@ -0,0 +1,8 @@
+// src/Main/java/Main.java
+package Main.java;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from Dockerized Java Application!. This is my first docker image");
+    }
+}
